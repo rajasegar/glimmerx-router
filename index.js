b1753a24e107b42c45589f00c0c976acb5949d81
Rajasegar <rajasegar.c@gmail.com>
2020-12-10 17:27:19 +0530

    chore: new esm compatible build

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-import Router from './src/Router';
-import Route from './src/Route';
-import Link from './src/Link';
-
-export {
-  Router,
-  Route,
-  Link
-};
+export { default as Router } from './src/Router';
+export { default as Route } from './src/Route';
+export { default as Link } from './src/Link';
